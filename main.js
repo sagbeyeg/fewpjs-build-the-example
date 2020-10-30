5c839a10d7680115f27c1e3e9bb01e68d3c12c88
sagbeyeg <agbeyegbesisan@gmail.com>
2020-10-30 12:47:07 -0400

    Completed deliverables, working to unlike the heart

--- main.js
+++ main.js
@@ -3,9 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+  function toggleLike() {
+    mimicServerCall
+    document.addEventListener("click", e => {
+      if (e.target.matches('.like-glyph')) {
+        const user = e.target.parentElement.parentElement.parentElement;
+        console.log(user);
+        const like = user.querySelector('.like-glyph');
+        console.log(like);
 
+        mimicServerCall()
+        .then(function(obj) {
+          console.log(obj)
+          toggleLikeColor(like)
+        })
+        .catch(function(error) {
+          console.log(error)
+          const modal = document.getElementById("modal")
+          modal.innerHTML = error
+          const notHidden = modal.className = ''
+          setTimeout(function() {
+            modal.className = 'hidden';
+          }, 5000)
+        })
+      }
+    }
+  )}
 
+  // document.addEventListener("click", toggleLikeColor(like))
 
+  toggleLike()
+
+  function toggleLikeColor(element) {
+    if (element.innerHTML === EMPTY_HEART) {
+      element.className = "activated-heart"
+      element.innerHTML = FULL_HEART
+    } else {
+      element.innerHTML = EMPTY_HEART
+      element.className = ""
+    }
+  }
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
